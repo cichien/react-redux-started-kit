ba503d459c8d12edd7ce06efd4cdaa9ef69aee61
Plane <planetw.git@gmail.com>
2016-11-28 23:41:50 +0800

    Add todo action api and dispatch function

--- src/actions/todo.jsx
+++ src/actions/todo.jsx
@@ -1,3 +1,25 @@
 export const FETCH_TODO = 'FETCH_TODO';
 export const FETCH_TODO_SUCCESS = 'FETCH_TODO_SUCCESS';
 export const FETCH_TODO_FAIL = 'FETCH_TODO_FAIL';
+
+export const fetchTodo = () => ({
+  type: FETCH_TODO,
+});
+
+export const fetchTodoApi = () => (
+  fetch('http://localhost:3000/todos')
+    .then(res => res.json())
+);
+
+export const ADD_TODO = 'ADD_TODO';
+export const ADD_TODO_SUCCESS = 'ADD_TODO_SUCCESS';
+export const ADD_TODO_FAIL = 'ADD_TODO_FAIL';
+
+export const addTodo = () => ({
+  type: ADD_TODO,
+});
+
+export const addTodoApi = () => (
+  fetch('http://localhost:3000/todos')
+    .then(res => res.json())
+);
