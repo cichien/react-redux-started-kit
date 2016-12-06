d35f24a07353296b059cd23e9cab6b13e18c610e
Plane <planetw.git@gmail.com>
2016-12-06 19:33:24 +0800

    Add todo input submission unit test

--- src/components/TodoInput/TodoInput.test.jsx
+++ src/components/TodoInput/TodoInput.test.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { mount } from 'enzyme';
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 import TodoInput from './';
 
 describe('Components: TodoInput', () => {
-  const wrapper = render(
-    <TodoInput />
+  const props = {
+    addTodo: sinon.spy(),
+  }
+  const  wrapper = mount(
+    <TodoInput {...props}/>
   );
+
 
   it('should have a <from> tag', () => {
     expect(wrapper.find('form').length).to.equal(1);
@@ -15,5 +20,11 @@ describe('Components: TodoInput', () => {
 
   it('should have a <Input> tag', () => {
     expect(wrapper.find('input').length).to.equal(1);
+  });
+
+  it('should call onTodoSubmit on form submission', () => {
+    expect(wrapper.props().addTodo).to.be.defined;
+    wrapper.simulate('submit');
+    expect(props.addTodo.calledOnce).to.equal(true);
   });
 });
